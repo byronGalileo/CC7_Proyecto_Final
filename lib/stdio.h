5da2bf1e36b8c15073d6f907b728f19845d559d8
Bysonts <bhaldairgg@gmail.com>
2025-05-02 20:27:30 -0600

    feat(Lib): :heavy_plus_sign: agrega carpeta lib en separado

---
+++ lib/stdio.h
@@ -0,0 +1,9 @@
+#ifndef STDIO_H
+#define STDIO_H
+
+void to_string(int num, char *buffer);
+int to_int(char *s);
+void PRINT(const char *format, ...);
+void READ(const char *format, ...);
+
+#endif // STDIO_H 
