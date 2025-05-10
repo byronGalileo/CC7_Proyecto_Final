30dc57a63674a7793d5c719b73ac0a556f7ea4ca
Bysonts <bhaldairgg@gmail.com>
2025-05-09 19:31:18 -0600

    feat(content swiching): :zap: verify if the swich return to the main

--- process_letters_v2/process1.c
+++ process_letters_v2/process1.c
@@ -8,6 +8,13 @@ void main() {
         for (char c = 'a'; c <= 'z'; c++) {
             PRINT("%c \n", c);
             delay_loop();
+            if (c == "k"){
+                asm volatile (
+                    "ldr sp, =0x80000000\n"   // Stack de proceso 1
+                    "ldr lr, =0x80000000\n"   // Código de proceso 1
+                    "movs pc, lr\n"
+                );
+            }
         }
     }
 }
--- process_numbers/process2.c
+++ process_numbers/process2.c
@@ -8,6 +8,13 @@ void main() {
         for (int i = 0; i < 10; i++) {
             PRINT("%d \n", i);
             delay_loop();
+            if (i == 9){
+                asm volatile (
+                    "ldr sp, =0x80000000\n"   // Stack de proceso 1
+                    "ldr lr, =0x80000000\n"   // Código de proceso 1
+                    "movs pc, lr\n"
+                );
+            }
         }
     }
 }
